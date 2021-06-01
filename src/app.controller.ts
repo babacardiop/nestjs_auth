13105c39fa95a8c51a820b7482f63161e64ae065
AfrikanCoder#1 <walababs@hotmail.com>
2021-06-01 21:25:09 +0000

    jwt based auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,19 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
-import { AuthenticatedGuard } from './auth/authenticated.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Get('login')
   login(@Request() req): any {
-    return { msg: 'Logged in !' };
+    return this.authService.login(req.user);
   }
 
-  @UseGuards(AuthenticatedGuard)
+  @UseGuards(JwtAuthGuard)
   @Get('protected')
   getHello(@Request() req): string {
     return req.user;
---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,18 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      secretOrKey: 'SECRET',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: any) {
+    return {
+      id: payload.sub,
+      name: payload.name,
+    };
+  }
+}
